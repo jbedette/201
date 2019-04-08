ea011e60f544db1592e924ebf39c9cb448f1d687
John Bedette <jbedette@augusta.cs.pdx.edu>
2019-04-07 19:20:35 -0700

    midway

---
+++ 4-9/Lab1Files/tan.c
@@ -0,0 +1,17 @@
+/* main returns an integer */
+
+#include <stdio.h>
+
+int
+main(int argc, char *argv[])
+{
+    /* printf is our output function;
+       by default, writes to standard out */
+    /* printf returns an integer, but we ignore that */ 
+    printf("hello, world\n");
+    printf("the tangent of 90 is: %d", double tan(90));
+
+    /* return 0 to indicate all went well */
+    return(0); 
+}
+
